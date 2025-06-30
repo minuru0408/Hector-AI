aa5a931d22685b96fedf3a11fdcd5c836883c9bd
Your Name <your@email.com>
2025-06-30 16:21:58 +0900

    dsa

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('dist/index.html'); // Parcel will build into /dist/
+}
+
+app.whenReady().then(createWindow);
