599220807a3b93661315ae9aa5e42fc8d17600bb
Your Name <your@email.com>
2025-06-30 22:06:54 +0900

    fds

--- main.js
+++ main.js
@@ -4,15 +4,13 @@ const path = require('path');
 function createWindow () {
   const win = new BrowserWindow({
     width: 1280,
-    height: 800,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: false,
-      contextIsolation: true
-    }
+    },
   });
 
-  win.loadFile('dist/index.html'); // Parcel will build into /dist/
+  win.loadFile('dist/index.html');
 }
 
 app.whenReady().then(createWindow);
