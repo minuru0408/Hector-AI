765130ccc614b2b0a1bb9601ea075a2990bbe405
Your Name <your@email.com>
2025-06-30 22:36:45 +0900

    fds

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Load using file protocol
+  win.loadFile(path.join(__dirname, '../dist/index.html'));
+  
+  // Open DevTools for debugging
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
