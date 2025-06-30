cda8d07e48e067ac0d33f3b8e4b1edd77de965bb
Your Name <your@email.com>
2025-06-30 22:35:30 +0900

    fds

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,12 +2,7 @@ import GL from './gl';
 
 console.log('Initializing application...');
 
-// Create instance of GL class
 const gl = new GL();
 console.log('GL instance created:', gl);
 
-// Expose to window to prevent tree-shaking
 window.gl = gl;
-
-// Export for potential use elsewhere
-export default gl;
