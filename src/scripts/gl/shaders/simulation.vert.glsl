8d945970f3f8b88bfd0989d20d7670eb8947bdc7
MINURU <minuru0408@gmail.com>
2025-06-30 23:22:58 +0900

    Fix shader compilation

--- src/scripts/gl/shaders/simulation.vert.glsl
+++ src/scripts/gl/shaders/simulation.vert.glsl
@@ -1,9 +1,8 @@
-#version 100
 varying vec2 vUv;
-attribute vec2 position;
 
 void main() {
   vUv = uv;
-  
-  gl_Position = vec4(position, 0.0, 1.0);
+
+  // Use the built in vertex position for the fullscreen quad
+  gl_Position = vec4(position, 1.0);
 }
